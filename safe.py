54a132d61e163344eb195c1338684e0e031b316d
xeny <44981148+1x6@users.noreply.github.com>
2021-05-30 20:50:17 +0100

    removed on msg, shortened timings, added auto lifesaver

--- safe.py
+++ safe.py
@@ -19,30 +19,6 @@ with open("auto-sell.json") as json_data_file2:
 async def on_ready():
     print(f'Danked has detected user {client.user.name}#{client.user.discriminator}.')
 
-
-@client.event
-async def on_message(message):
-    # event react
-    if 'in the next 10 seconds' in message.content:
-        if message.channel.id == int(config['channel_id']):
-            getPhrase = message.content.split("`")
-            await asyncio.sleep(.25)
-            await message.channel.send(getPhrase[1])
-            print(getPhrase[1]) # this is the bit with whatever they want us to say in
-    
-    # allow control from whitelisted user(s)
-    if '.danked control' in message.content:
-        if message.author.id == int(config['control_whitelist']):
-            cmd2say = message.content.split(":")
-            if cmd2say[2] == f"{client.user.id}":
-                await message.channel.send(cmd2say[1])
-            
-    
-    # search react
-    # lol not done
-
-    await client.process_commands(message)
-
 @client.command()
 async def start(ctx):
     channel = client.get_channel(int(config['channel_id']))
@@ -50,6 +26,7 @@ async def start(ctx):
         # begging vars
     pm_count = 0
     money_holder_count = 0
+    auto_lifesaver_count = 0
     
     await asyncio.sleep(2)
 
@@ -107,38 +84,13 @@ async def start(ctx):
 
         await asyncio.sleep(random.uniform(1,3))
 
-        # pm msg
+        # dig
         async with ctx.typing():
             type_time = random.uniform(0.5, 1)
             await asyncio.sleep(type_time)
         await channel.send('pls dig')
 
         await asyncio.sleep(random.uniform(.2, 1))
-
-        # auto buy laptop
-        if pm_count == int(config['laptop_buying_frequency']) :
-            await asyncio.sleep(.25)
-            async with ctx.typing():
-                type_time = random.uniform(0.5, 1)
-                await asyncio.sleep(type_time)
-
-            await channel.send('pls withdraw 5000')
-
-            await asyncio.sleep(random.uniform(.5, 2.4))
-
-            async with ctx.typing():
-                type_time = random.uniform(0.5, 1)
-                await asyncio.sleep(type_time)
-
-            await channel.send('pls buy laptop')
-
-            pm_count = 0
-        else :
-            pm_count += 1
-        # end auto laptop
-
-
-        await asyncio.sleep(1.69)
 
         # money holder
         if config['money_holder_enabled'] == 'true':
@@ -160,7 +112,19 @@ async def start(ctx):
             else :
                 money_holder_count += 1
 
-        await asyncio.sleep(5)
+        await asyncio.sleep(1)
+
+        # auto lifesaver
+        if config['auto_lifesaver_enabled'] == 'true':
+            if auto_lifesaver_count == int(config['auto_lifesaver_frequency']) :
+                async with ctx.typing():
+                    type_time = random.uniform(0.5, 1)
+                    await asyncio.sleep(type_time)
+                await channel.send(f'pls use lifesaver')
+
+                auto_lifesaver_count = 0
+            else :
+                auto_lifesaver_count += 1
 
         # fishing
         async with ctx.typing():
@@ -168,7 +132,7 @@ async def start(ctx):
             await asyncio.sleep(type_time)
         await channel.send('pls fish')
 
-        await asyncio.sleep(random.uniform(4, 6)) # wait longer so the event react thing can run in case of type
+        await asyncio.sleep(random.uniform(1, 2))
 
         # hunting
         async with ctx.typing():
@@ -176,7 +140,7 @@ async def start(ctx):
             await asyncio.sleep(type_time)
         await channel.send('pls hunt')
 
-        await asyncio.sleep(random.uniform(4, 6)) # wait longer so the event react thing can run in case of type
+        await asyncio.sleep(random.uniform(1, 2))
         
         await asyncio.sleep(random.uniform(45, 50))
 
